9a9d71386bc16dd9ea1b04b2b0f51dc784f9180d
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2025-04-20 15:02:35 +0700

    integrated template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
-
   return (
-    <div>Hello world!</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
