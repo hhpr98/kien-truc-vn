87ec10a4ba9e7613142ef73d47740335f6073df2
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2025-04-20 18:22:18 +0700

    update product description

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,60 +1,52 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react';
+
 import { useParams } from 'react-router-dom'
+import projects from '../assets/projects.json';
 
 const ProductDetail = () => {
-  const { id } = useParams()
-  console.log(id)
+  const { id } = useParams();
+
+  const [project, setProject] = useState(null);
+
+  useEffect(() => {
+    const projectData = projects[id];
+    setProject(projectData);
+  }, [id]);
 
   return (<Fragment>
-    <section class="product-detail section-padding">
-      <div class="container">
-        <div class="row">
+    <section className="product-detail section-padding">
+      <div className="container">
+        <div className="row">
 
-          <div class="col-lg-6 col-12">
-            <div class="product-thumb">
-              <img src="images/product/evan-mcdougall-qnh1odlqOmk-unsplash.jpeg" class="img-fluid product-image" alt="" />
+          <div className="col-lg-6 col-12">
+            <div className="product-thumb">
+              <img src={project?.projectMainURL} className="img-fluid product-image" alt="" />
             </div>
           </div>
 
-          <div class="col-lg-6 col-12">
-            <div class="product-info d-flex">
+          <div className="col-lg-6 col-12">
+            <div className="product-info d-flex">
               <div>
-                <h2 class="product-title mb-0">Tree pot</h2>
-
-                <p class="product-p">Original package design from house</p>
+                <h2 className="product-title mb-0">{project?.projectName}</h2>
+                <p className="product-p">{project?.productDetail}</p>
               </div>
-
-              <small class="product-price text-muted ms-auto mt-auto mb-5">$25</small>
+              <small className="product-price text-muted ms-auto mt-auto mb-5">$25</small>
             </div>
 
-            <div class="product-description">
-
-              <strong class="d-block mt-4 mb-2">Description</strong>
-
-              <p class="lead mb-5">Over three years in business, We’ve had the chance to work on a variety of projects, with companies</p>
+            <div className="product-description">
+              <strong className="d-block mt-4 mb-2">Mô tả</strong>
+              <p className="lead mb-5">{project?.projectDescription}</p>
             </div>
 
-            <div class="product-cart-thumb row">
-              <div class="col-lg-6 col-12">
-
-                <select class="form-select cart-form-select" id="inputGroupSelect01">
-                  <option selected>Quantity</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                </select>
-              </div>
-
-              <div class="col-lg-6 col-12 mt-4 mt-lg-0">
-                <button type="submit" class="btn custom-btn cart-btn" data-bs-toggle="modal" data-bs-target="#cart-modal">Add to Cart</button>
+            <div className="product-cart-thumb row">
+              <div className="col-lg-6 col-12 mt-4 mt-lg-0">
+                <button type="submit" className="btn custom-btn cart-btn" data-bs-toggle="modal" data-bs-target="#cart-modal">Liên hệ báo giá</button>
               </div>
 
               <p>
-                <a href="#" class="product-additional-link">Details</a>
+                <a href="#" className="product-additional-link">Details</a>
 
-                <a href="#" class="product-additional-link">Delivery and Payment</a>
+                <a href="#" className="product-additional-link">Delivery and Payment</a>
               </p>
             </div>
 
@@ -64,86 +56,86 @@ const ProductDetail = () => {
       </div>
     </section>
 
-    <section class="related-product section-padding border-top">
-      <div class="container">
-        <div class="row">
+    <section className="related-product section-padding border-top">
+      <div className="container">
+        <div className="row">
 
-          <div class="col-12">
-            <h3 class="mb-5">You might also like</h3>
+          <div className="col-12">
+            <h3 className="mb-5">You might also like</h3>
           </div>
 
-          <div class="col-lg-4 col-12 mb-3">
-            <div class="product-thumb">
+          <div className="col-lg-4 col-12 mb-3">
+            <div className="product-thumb">
               <a href="product-detail.html">
-                <img src="images/product/evan-mcdougall-qnh1odlqOmk-unsplash.jpeg" class="img-fluid product-image" alt="" />
+                <img src="images/product/evan-mcdougall-qnh1odlqOmk-unsplash.jpeg" className="img-fluid product-image" alt="" />
               </a>
 
-              <div class="product-top d-flex">
-                <span class="product-alert me-auto">New arrival</span>
+              <div className="product-top d-flex">
+                <span className="product-alert me-auto">New arrival</span>
 
-                <a href="#" class="bi-heart-fill product-icon"></a>
+                <a href="#" className="bi-heart-fill product-icon"></a>
               </div>
 
-              <div class="product-info d-flex">
+              <div className="product-info d-flex">
                 <div>
-                  <h5 class="product-title mb-0">
-                    <a href="product-detail.html" class="product-title-link">Tree pot</a>
+                  <h5 className="product-title mb-0">
+                    <a href="product-detail.html" className="product-title-link">Tree pot</a>
                   </h5>
 
-                  <p class="product-p">Original package design from house</p>
+                  <p className="product-p">Original package design from house</p>
                 </div>
 
-                <small class="product-price text-muted ms-auto mt-auto mb-5">$25</small>
+                <small className="product-price text-muted ms-auto mt-auto mb-5">$25</small>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-4 col-12 mb-3">
-            <div class="product-thumb">
+          <div className="col-lg-4 col-12 mb-3">
+            <div className="product-thumb">
               <a href="product-detail.html">
-                <img src="images/product/jordan-nix-CkCUvwMXAac-unsplash.jpeg" class="img-fluid product-image" alt="" />
+                <img src="images/product/jordan-nix-CkCUvwMXAac-unsplash.jpeg" className="img-fluid product-image" alt="" />
               </a>
 
-              <div class="product-top d-flex">
-                <span class="product-alert">Low Price</span>
+              <div className="product-top d-flex">
+                <span className="product-alert">Low Price</span>
 
-                <a href="#" class="bi-heart-fill product-icon ms-auto"></a>
+                <a href="#" className="bi-heart-fill product-icon ms-auto"></a>
               </div>
 
-              <div class="product-info d-flex">
+              <div className="product-info d-flex">
                 <div>
-                  <h5 class="product-title mb-0">
-                    <a href="product-detail.html" class="product-title-link">Fashion set</a>
+                  <h5 className="product-title mb-0">
+                    <a href="product-detail.html" className="product-title-link">Fashion set</a>
                   </h5>
 
-                  <p class="product-p">Costume package</p>
+                  <p className="product-p">Costume package</p>
                 </div>
 
-                <small class="product-price text-muted ms-auto mt-auto mb-5">$35</small>
+                <small className="product-price text-muted ms-auto mt-auto mb-5">$35</small>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-4 col-12">
-            <div class="product-thumb">
+          <div className="col-lg-4 col-12">
+            <div className="product-thumb">
               <a href="product-detail.html">
-                <img src="images/product/nature-zen-3Dn1BZZv3m8-unsplash.jpeg" class="img-fluid product-image" alt="" />
+                <img src="images/product/nature-zen-3Dn1BZZv3m8-unsplash.jpeg" className="img-fluid product-image" alt="" />
               </a>
 
-              <div class="product-top d-flex">
-                <a href="#" class="bi-heart-fill product-icon ms-auto"></a>
+              <div className="product-top d-flex">
+                <a href="#" className="bi-heart-fill product-icon ms-auto"></a>
               </div>
 
-              <div class="product-info d-flex">
+              <div className="product-info d-flex">
                 <div>
-                  <h5 class="product-title mb-0">
-                    <a href="product-detail.html" class="product-title-link">Juice Drinks</a>
+                  <h5 className="product-title mb-0">
+                    <a href="product-detail.html" className="product-title-link">Juice Drinks</a>
                   </h5>
 
-                  <p class="product-p">Nature made another world</p>
+                  <p className="product-p">Nature made another world</p>
                 </div>
 
-                <small class="product-price text-muted ms-auto mt-auto mb-5">$45</small>
+                <small className="product-price text-muted ms-auto mt-auto mb-5">$45</small>
               </div>
             </div>
           </div>
