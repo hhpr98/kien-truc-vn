e1d68ae6873bc90180468560695e1c0cd777e364
Hoa Nguyen <hoa.nguyen@opswat.com>
2025-05-18 16:33:07 +0700

    change upload button background color

--- src/pages/ImageUpload.jsx
+++ src/pages/ImageUpload.jsx
@@ -91,11 +91,12 @@ const ImageUpload = () => {
             accept="image/*"
             onChange={handleImageChange}
             className="mb-4"
-          />{" "}
+          />
+          {" "}
           <button
             onClick={handleUpload}
             disabled={!image}
-            className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50"
+            className="bg-primary text-white px-4 py-2 rounded disabled:opacity-50"
           >
             Upload Image
           </button>
